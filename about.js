c82011394060428c4b5565c2c7e7060dff9228a4
Ofek Shpirer <130273989+ofekshp@users.noreply.github.com>
2023-08-30 23:10:12 +0300

    Ofek Shpirer
    hello

--- about.js
+++ about.js
@@ -16,18 +16,90 @@ function CategoryPage(name) {
 $(document).ready(function () {
   $("#searchForm").submit(function (event) {
     event.preventDefault();
-    const name = $("#searchInput").val();
-    if (["bottles", "cup", "cups", "straws","straw"].includes(name)) {
+    const name1 = $("#searchInput").val();
+    if (["bottles","cups","straws"].includes(name1)) {
     $.ajax({
-      url: "http://localhost:3000/product/category/" + name,
-      method: "GET",
-      success: function (response) {
-        window.location.href = "bottles.html?category=" + name;
-      },
-      error: function (xhr) {
+      url: "http://localhost:3000/product/graph/" + name1,
+      type: "GET",
+      data: JSON.stringify({
+        name1
+      }),
+      success: function (response) {       
+        const productName = name1;
+        const countProduct = response.countProduct;
+        const removeAble = d3.select("#GenderChart");
+// Remove all child elements within the SVG
+        removeAble.selectAll("*").remove();
+        // Set up the dimensions for the SVG container
+        const svgWidth = 650;
+        const svgHeight = 470;
+        const margin = { top: 40, right: 40, bottom: 80, left: 60 };
+        const width = svgWidth - margin.left - margin.right;
+        const height = svgHeight - margin.top - margin.bottom;
+    
+        // Create the SVG container for the chart
+        const svg = d3.select("#GenderChart")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+    
+        // Create a group within the SVG for the charting area
+        const chart = svg.append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+        // Define a title for the graph
+        const graphTitle = "Product count";
+    
+        // Create a text element for the title and position it at the center
+        svg.append("text")
+            .attr("class", "graph-title")
+            .attr("x", svgWidth / 2)
+            .attr("y", margin.top / 2)
+            .text(graphTitle);
+    
+        // Set up scales for x and y axes
+        const xScale = d3.scaleBand()
+            .domain([productName])
+            .range([0, width])
+            .paddingInner(0.1)
+            .paddingOuter(0.2);
+    
+        const yScale = d3.scaleLinear()
+            .domain([0, countProduct])
+            .nice()
+            .range([height, 0]);
+    
+        // Create the column for the chart
+        chart.selectAll(".bar")
+            .data([productName]) // Pass the product name as an array
+            .enter().append("rect")
+            .attr("class", "barGender")
+            .attr("x", name => xScale(name) + xScale.bandwidth() / 2.2)
+            .attr("y", yScale(countProduct))
+            .attr("width", xScale.bandwidth()/10)
+            .attr("height", name => height - yScale(countProduct));
+    
+        // Create x and y axes
+        const xAxis = d3.axisBottom(xScale);
+    
+        // Append x axis to the charting area
+        const xAxisGroup = chart.append("g")
+            .attr("class", "x-axis")
+            .attr("transform", "translate(0," + (height) + ")")
+            .call(xAxis);
+    
+        // Add a class to the x-axis labels
+        xAxisGroup.selectAll("text")
+            .attr("class", "x-axis-label");
+    
+        // Append y axis to the charting area
+        chart.append("g")
+            .attr("class", "y-axis")
+            .call(d3.axisLeft(yScale));
+    },
+    error: function (xhr) {
         console.log(xhr.responseText);
         // Handle the error
-      },
+    },
     });}
 
  $.ajax({
